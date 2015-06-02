3b8e1babc6b82f02546b10808b7c68b96ef2d89f
Vladimir Enchev <vladimir.enchev@telerik.com>
2015-06-02 13:46:32 +0300

    Remove duplicated directory

--- libsrc/Android/Common/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Work\GitHub\Telerik\TelerikNEXT\lib\Android\Common\bin\AndroidManifest.xml : \
-D:\Work\GitHub\Telerik\TelerikNEXT\lib\Android\Common\AndroidManifest.xml \
--- libsrc/Android/primitives/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Work\GitHub\Telerik\TelerikNEXT\lib\Android\Primitives\bin\AndroidManifest.xml : \
-D:\Work\GitHub\Telerik\TelerikNEXT\lib\Android\Primitives\AndroidManifest.xml \
