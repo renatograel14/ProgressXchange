977914cfda3ed9d6b3071bd1b90edbc7340bc2ff
Renato Teixeira <rteixeir@progress.com>
2015-08-26 19:06:07 +0000

    Changes from Screen Builder

--- Progress XChange Hybrid/components/home/index.js
+++ Progress XChange Hybrid/components/home/index.js
@@ -1,8 +1,35 @@
 'use strict';
 
 app.home = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    onShow: function () {},
+    afterShow: function () {},
+    locationService: {
+        initLocation: function () {
+            var map,
+                mapOptions,
+                streetView;
+
+            if (typeof google === "undefined") {
+                return;
+            }
+
+
+            mapOptions = {
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                zoomControl: true,
+                zoomControlOptions: {
+                    position: google.maps.ControlPosition.LEFT_BOTTOM
+                },
+                mapTypeControl: false,
+                streetViewControl: false
+            };
+
+            map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+            google.maps.event.trigger(map, "resize");	
+
+        }
+    }
 });
 
 // START_CUSTOM_CODE_home
